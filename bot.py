54c2cce0e13e6709ed79d25cafe9ec3af660eed1
Semyeong Hong <ca711207@gmail.com>
2021-09-27 23:37:09 +0900

    feat: public functions

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
-print('Hello')
+import telegram
+
+# =================== Global Variables ===================
+TOKEN = '1956707809:AAF7QQ2c6Wrcxy-iemmcF-zhF7cLzFjTIIg'
+CHAT_ID = '@uoft_news_bot_channel'
+
+
+# =================== Public Functions ===================
+def create_bot():
+    """ Creates a bot with telegram module. 
+    
+    Return
+        A telegram bot created with a bot token.
+    """
+    bot = telegram.Bot(token=TOKEN)
+    return bot
+
+
+def send_message(bot, message: str) -> None:
+    """ Makes a bot to send message to a uoft news channel.
+    
+    Params
+        bot: A bot that sends a message
+        message: A text chunk that a bot needs to send
+    """
+    bot.send_message(chat_id=CHAT_ID, text=message)
