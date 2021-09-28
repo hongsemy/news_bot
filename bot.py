c98326825b4d38f6f83b3747ebb551177526ea7d
hongsemy <semyeong.hong@mail.utoronto.ca>
2021-09-28 12:18:47 +0900

    refactor: Change logic to prevent possible exceptions

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import telegram
-import crawler
 
 # =================== Global Variables ===================
 TOKEN = '1956707809:AAF7QQ2c6Wrcxy-iemmcF-zhF7cLzFjTIIg'
@@ -26,17 +25,13 @@ def send_message(bot, message: str) -> None:
     """
     bot.send_message(chat_id=CHAT_ID, text=message)
 
+
 def send_photo(bot, img_url: str, caption: str) -> None:
+    """ Makes a bot to send photo messge to a uoft news channel.
+    
+    Params
+        bot: A bot that sends a photo message
+        img_url: An URL to an image that a bot needs to send
+        caption: A text chunk that a bot needs to send
+    """
     bot.sendPhoto(chat_id=CHAT_ID, photo=img_url, caption=caption)
-
-if __name__ == '__main__':
-    info = {
-        'title': 'Higher levels of organic pollutants found in homes located near natural gas wells: U of T study',
-        'url': 'https://www.utoronto.ca/news/higher-levels-organic-pollutants-found-homes-located-near-natural-gas-wells-u-t-study',
-        'thumbnail': 'https://www.utoronto.ca/sites/default/files/styles/750x500/public/GettyImages-130904081-crop.jpg?itok=H6IvhXFR',
-        'date': 'September 23'  
-    }
-
-    bot = create_bot()
-    caption = '"' + info['title'] + '"' + '\n\n' + info['date'] + '\n\n' + info['url']
-    send_photo(bot, info['url'], caption)
